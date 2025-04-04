41f21645371704c2e18a6ad177f0edab23c68c93
Dave Howard <zenfosec@gmail.com>
2025-04-04 11:17:51 -0700

    Update nmap_http_parse.py
    Updating to python3

--- nmap_http_parse.py
+++ nmap_http_parse.py
@@ -1,34 +1,53 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
-# Finds HTTP/HTTPS services and prints URLs
-#
-# 6/14/13 - zenfosec
+# Finds HTTP/HTTPS services and prints URLs with hostnames if available, otherwise IP address
+# Updating 2013 python2 script to python3
 
 import sys
 import re
+import socket
+
+def get_hostname(ip_address):
+    """
+    Perform reverse DNS lookup to get the hostname for a given IP address.
+    Returns the hostname if found, otherwise returns None.
+    """
+    try:
+        hostname = socket.gethostbyaddr(ip_address)[0]
+        return hostname
+    except socket.herror:
+        return None
 
 if len(sys.argv) != 2:
-    print "Usage: http_parse.py <gnmap_file>"
+    print("Usage: http_parse.py <gnmap_file>")
     sys.exit()
 
-infile = open(sys.argv[1], "r")
+# Open the input file using a context manager for better resource management
+with open(sys.argv[1], "r") as infile:
+    port_dict = {}
 
-port_dict = {}
-
-for line in infile:
-    if re.search("http", line, flags=re.IGNORECASE):
-        httpentries = re.findall(" \d*/open/[\w\|\-\. \?\(\)]*?/\w*/[\w\|\-\. \?\(\)]*http[\w\|\-\. \|\?\(\)]*/[\w\|\-\. \?\(\)]*/", line, flags=re.IGNORECASE)
-        #httpentries = line.split(", ")
-        for entry in httpentries:
-            if re.search("ssl|https", entry, flags=re.IGNORECASE):
-                sys.stdout.write("https://")
-                sys.stdout.write(line.split(" ")[1])
-                sys.stdout.write(":")
-                sys.stdout.write(entry.split("/")[0].strip())
-                print
-            else:
-                sys.stdout.write("http://")
-                sys.stdout.write(line.split(" ")[1])
-                sys.stdout.write(":")
-                sys.stdout.write(entry.split("/")[0].strip())
-                print
+    for line in infile:
+        if re.search("http", line, flags=re.IGNORECASE):
+            httpentries = re.findall(
+                r" \d*/open/[\w\|\-\. \?\(\)]*?/\w*/[\w\|\-\. \?\(\)]*http[\w\|\-\. \|\?\(\)]*/[\w\|\-\. \?\(\)]*/",
+                line,
+                flags=re.IGNORECASE,
+            )
+            for entry in httpentries:
+                ip_address = line.split(" ")[1]
+                port = entry.split("/")[0].strip()
+                
+                # Perform reverse DNS lookup
+                hostname = get_hostname(ip_address)
+                
+                # Use hostname if available; fallback to IP address otherwise
+                if re.search("ssl|https", entry, flags=re.IGNORECASE):
+                    if hostname:
+                        print(f"https://{hostname}:{port}")
+                    else:
+                        print(f"https://{ip_address}:{port}")
+                else:
+                    if hostname:
+                        print(f"http://{hostname}:{port}")
+                    else:
+                        print(f"http://{ip_address}:{port}")
